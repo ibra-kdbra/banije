3e04e95e30888b147f16597694f7d799486c59ee
ibra-kdbra <git-repo-develop@outlook.com>
2024-11-22 23:12:59 +0300

    commented for development

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: '',
+  // site: 'https://fuwari.vercel.app/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
