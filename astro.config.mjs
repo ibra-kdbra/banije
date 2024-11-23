5275ce744da6a90b644a1f36cfbc143381f80b86
ibra-kdbra <135609275+ibra-kdbra@users.noreply.github.com>
2024-11-23 23:55:56 +0300

    deploying test

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://fuwari.vercel.app/',
+  site: 'https://banije.vercel.app/',
   base: '/',
   trailingSlash: 'always',
   integrations: [
