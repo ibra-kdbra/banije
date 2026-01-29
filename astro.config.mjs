41c9c8cf5c3d9b5924878c33c44ad9e9eda432fc
ibra-kdbra <git-repo-develop@outlook.com>
2026-01-30 02:01:11 +0300

    config: update site configuration for multi-language support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import swup from "@swup/astro";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components"; /* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
@@ -26,7 +27,12 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "static",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 	site: "https://banije.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
