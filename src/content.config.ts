c768ff58e40124ac26f8d7be8b91033b1bc541ad
ibra-kdbra <git-repo-develop@outlook.com>
2026-05-21 20:37:37 +0300

    feat(content): create content.config.ts using new Astro glob loaders

---
+++ src/content.config.ts
@@ -0,0 +1,83 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const postsCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/posts" }),
+  schema: z.object({
+    title: z.string(),
+    published: z.date(),
+    updated: z.date().optional(),
+    draft: z.boolean().optional().default(false),
+    description: z.string().optional().default(""),
+    image: z.string().optional().default(""),
+    tags: z
+      .array(z.string().trim().min(1))
+      .optional()
+      .default([]),
+    category: z
+      .string()
+      .trim()
+      .optional()
+      .nullable()
+      .default(""),
+    lang: z.string().optional().default(""),
+    originalSlug: z.string().optional(),
+    series: z
+      .object({
+        name: z.string().trim(),
+        order: z.number().int().min(1),
+      })
+      .optional(),
+
+    /* For internal use */
+    prevTitle: z.string().default(""),
+    prevSlug: z.string().default(""),
+    nextTitle: z.string().default(""),
+    nextSlug: z.string().default(""),
+  }),
+});
+
+const translateCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/translate" }),
+  schema: z.object({
+    title: z.string(),
+    published: z.date(),
+    updated: z.date().optional(),
+    draft: z.boolean().optional().default(false),
+    description: z.string().optional().default(""),
+    image: z.string().optional().default(""),
+    tags: z
+      .array(z.string().trim().min(1))
+      .optional()
+      .default([]),
+    category: z
+      .string()
+      .trim()
+      .optional()
+      .nullable()
+      .default(""),
+    lang: z.string(),
+    originalSlug: z.string(),
+    series: z
+      .object({
+        name: z.string().trim(),
+        order: z.number().int().min(1),
+      })
+      .optional(),
+    prevTitle: z.string().default(""),
+    prevSlug: z.string().default(""),
+    nextTitle: z.string().default(""),
+    nextSlug: z.string().default(""),
+  }),
+});
+
+const specCollection = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/spec" }),
+  schema: z.object({}),
+});
+
+export const collections = {
+  posts: postsCollection,
+  translate: translateCollection,
+  spec: specCollection,
+};
